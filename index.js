9d8576c6f22147c11f2efda809045faad18c2791
Cleogoncalves <santos.c@estudantes.ifg.edu.br>
2026-06-15 21:16:25 -0300

    aula15

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-const express =  require('express')
+const express = require("express")
 
 const app = express()
+
 app.use(express.json())
 
+app.get("/", ( req, res) => {
+    res.json ({mensagem: "servidor ta no ar!"})
 
-app.listen(3000,  () => {
-    console.log('Api rodando na porta 3000, ')
-
-    const alunos = []
-    app.get('/alunos', (pedido, resposta) => {
-        resposta.json(alunos)
-    })
-    app.post('/alunos', (pedido, resposta) => {
-
-
-        const aluno = {
-            matricula: pedido.body.matricula,
-            nome : pedido.body.matricula,
-            dataNasc : pedido.body.matricula ,
-            email : pedido.body.matricula
-        }
-        alunos.push (aluno) 
-        resposta.json(aluno)
-    })
 })
+app.get("/usua rios", (req,res) => {
+    res.json({usuarios: "toma aqui seus usuarios!"})
+})
+
+
+app.listen(3000, () => {
+     console.log("🚀  Hellow world !")
+})
+
